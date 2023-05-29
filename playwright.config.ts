4ab2fb9017e28b769737c516b78b2b159a103616
m.drobacgrujicic <m.drobacgrujicic@levi9.com>
2023-05-29 08:48:15 +0200

    added reporters

--- playwright.config.ts
+++ playwright.config.ts
@@ -29,7 +29,15 @@ const config: PlaywrightTestConfig = {
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [
+          ['html'],
+          ['list'],
+          ['monocart-reporter', {  
+              name: 'My Test Report',
+              outputFile: './test-results/report.html'
+        
+          }]
+        ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     baseURL: 'https://automationintesting.online/',
