2f3e14322549ecd1dce8766270b0370d783bb399
m.drobacgrujicic <m.drobacgrujicic@levi9.com>
2023-07-11 11:56:00 +0200

    added merged file

---
+++ merged-monocart-report.ts
@@ -0,0 +1,24 @@
+import { merge } from 'monocart-reporter';
+
+const reportDataList = [
+    process.cwd() + '/playwright-report-ExecuteInChrome-1_2/index.json',
+    process.cwd() + '/playwright-report-ExecuteInChrome-2_2/index.json',
+    process.cwd() + '/playwright-report-ExecuteInFF-1_2/index.json',
+    process.cwd() + '/playwright-report-ExecuteInFF-2_2/index.json'
+];
+
+merge(reportDataList, {
+    name: 'Merged monocart report',
+    outputFile: 'test-results/index.html',
+    trend: 'test-results/result.json',
+    attachmentPath: (currentPath: string) => {
+        const searchStr = '../test-results/';
+        const replaceStr = './data/';
+
+        if (currentPath.startsWith(searchStr)) {
+            return replaceStr + currentPath.slice(searchStr.length);
+        }
+
+        return currentPath;
+    }
+});
